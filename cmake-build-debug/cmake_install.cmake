da476bc2dce2b5026998abcc10d4b72842d0bee7
cj <ricezcj@gmail.com>
2017-02-12 13:25:13 -0600

    update

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/liufang/Documents/Courses/comp530/530HW2
+# Install script for directory: /Users/cj/Documents/17 Spring/comp 530/assignments/assign2/A2/530HW2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/liufang/Documents/Courses/comp530/530HW2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/cj/Documents/17 Spring/comp 530/assignments/assign2/A2/530HW2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
